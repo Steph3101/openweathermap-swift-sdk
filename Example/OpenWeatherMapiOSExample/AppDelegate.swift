18ad7207b57a7fb86861c2984cc19b89f1b6e105
Ulaş Sancak <ulas.sancak@hotmail.com.tr>
2017-04-10 01:41:18 +0300

    App transport security fix. API key initialisation.

--- Example/OpenWeatherMapiOSExample/AppDelegate.swift
+++ Example/OpenWeatherMapiOSExample/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import openweathermap_swift_sdk
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // #warning Change App ID
+        OpenWeatherMapClient.client(appID: "app_id")
         return true
     }
 
